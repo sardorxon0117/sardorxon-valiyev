8062991d39aeb824fefbf0f5d0d0916708bbef7f
Sardorxon Valiyev <sardorxonvvaliyev006@gmail.com>
2025-12-18 17:13:04 +0500

    Added CORS support for all origins

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Use express
 const express = require("express");
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 // Routes
 
 const pool = require("./config/db.js")
